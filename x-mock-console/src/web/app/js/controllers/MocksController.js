d35baec55258c15901e0e16586301c13db15c4e6
Tim Chang <tim.kri@gmail.com>
2014-06-01 13:29:50 +0800

    Mock Controller

--- x-mock-console/src/web/app/js/controllers/MocksController.js
+++ x-mock-console/src/web/app/js/controllers/MocksController.js
@@ -1,11 +1,25 @@
 define([], function() {
-	return ['$scope', '$http', function($scope, $http) {
-		// You can access the scope of the controller from here
-		$scope.welcomeMessage = 'hey this is myctrl2.js!';
+	return [ '$scope', '$http', function($scope, $http) {
+
+		$scope.mock = {
+			verb : "GET",
+			headers : {
+				contentEncoding : "UTF-8"
+			},
+			response : {
+				status : "200",
+				body : '{ "test" : "Response" }'
+			}
+		};
+
+		$scope.save = function() {
+			console.log($scope.mock);
+		};
 
 		// because this has happened asynchroneusly we've missed
 		// Angular's initial call to $apply after the controller has been loaded
-		// hence we need to explicityly call it at the end of our Controller constructor
+		// hence we need to explicityly call it at the end of our Controller
+		// constructor
 		$scope.$apply();
-	}];
+	} ];
 });
