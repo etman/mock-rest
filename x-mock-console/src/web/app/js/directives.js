1fb38a149ca68c6815d573b2a3b6a784162321bc
Tim Chang <tim.kri@gmail.com>
2014-06-04 00:31:58 +0800

    Handle enter event

--- x-mock-console/src/web/app/js/directives.js
+++ x-mock-console/src/web/app/js/directives.js
@@ -15,5 +15,17 @@ define(['angular', 'services'], function(angular, services) {
 				element[0].focus();
 			});
 		}
-	}});
+	}}).directive('ngEnter', function() {
+		return function(scope, element, attrs) {
+			element.bind("keydown keypress", function(event) {
+				if (event.which === 13) {
+					scope.$apply(function() {
+						scope.$eval(attrs.ngEnter);
+					});
+
+					event.preventDefault();
+				}
+			});
+		};
+	});
 });
